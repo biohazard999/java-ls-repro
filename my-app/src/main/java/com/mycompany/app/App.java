d9196221e72aed05e45adea74ff964d2eff238f9
Manuel Grundner <mgrundner@ranorex.com>
2018-06-22 11:01:14 +0200

    initial commit

---
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,17 @@
+package com.mycompany.app;
+
+import com.mycompany.app.foo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        new foo().main(args);
+        
+
+    }
+}
